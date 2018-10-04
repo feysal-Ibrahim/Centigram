59ebbc548ce755ce9a9cf1ecbc3344e3719635de
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-04 13:31:32 +0300

    create template file

--- centiapp/views.py
+++ centiapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse , Http404  # import the HttpResponse class from the django.http module.
-
+from peewee import DoesNotExist
 # Create your views here.
 
+def home (request):
+    return render(request,'index.html')
