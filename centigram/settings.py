5d9d73946bab1a851202bf9f9c9cd6c9ed9dae86
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-08 17:00:58 +0300

    create model class for comments,image and profile

--- centigram/settings.py
+++ centigram/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-
 import os
 import django_heroku
 import dj_database_url
@@ -23,7 +22,7 @@ DEBUG=config( 'DEBUG' , default=False , cast=bool )
 if config( 'MODE' ) == "dev":
     DATABASES={
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
             'NAME': config( 'DB_NAME' ) ,
             'USER': config( 'DB_USER' ) ,
             'PASSWORD': config( 'DB_PASSWORD' ) ,
@@ -45,130 +44,126 @@ DATABASES['default'].update( db_from_env )
 
 ALLOWED_HOSTS=config( 'ALLOWED_HOSTS' , cast=Csv( ) )
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR=os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join( BASE_DIR , 'staticfiles' )
+STATIC_URL='/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS=(
+    os.path.join( BASE_DIR , 'static' ) ,
 )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tx1^kypc!fmi&7#&+i%=(&!f-k#u7uzpn1m5y86!4a77nobg6h'
+SECRET_KEY='tx1^kypc!fmi&7#&+i%=(&!f-k#u7uzpn1m5y86!4a77nobg6h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS=[]
 
 # Application definition
 
-INSTALLED_APPS = [
-    'centiapp.apps.CentiappConfig',
+INSTALLED_APPS=[
+    'centiapp.apps.CentiappConfig' ,
     'bootstrap3' ,
     'bootstrap4' ,
-    'tinymce',
-    'pyuploadcare',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'tinymce' ,
+    'pyuploadcare' ,
+    'django.contrib.admin' ,
+    'django.contrib.auth' ,
+    'django.contrib.contenttypes' ,
+    'django.contrib.sessions' ,
+    'django.contrib.messages' ,
+    'django.contrib.staticfiles' ,
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE=[
     'whitenoise.middleware.WhiteNoiseMiddleware' ,
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware' ,
+    'django.contrib.sessions.middleware.SessionMiddleware' ,
+    'django.middleware.common.CommonMiddleware' ,
+    'django.middleware.csrf.CsrfViewMiddleware' ,
+    'django.contrib.auth.middleware.AuthenticationMiddleware' ,
+    'django.contrib.messages.middleware.MessageMiddleware' ,
+    'django.middleware.clickjacking.XFrameOptionsMiddleware' ,
 ]
 
-ROOT_URLCONF = 'centigram.urls'
+ROOT_URLCONF='centigram.urls'
 
-TEMPLATES = [
+TEMPLATES=[
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates' ,
+        'DIRS': [] ,
+        'APP_DIRS': True ,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
-            ],
-        },
-    },
+                'django.template.context_processors.debug' ,
+                'django.template.context_processors.request' ,
+                'django.contrib.auth.context_processors.auth' ,
+                'django.contrib.messages.context_processors.messages' ,
+                'django.template.context_processors.media' ,
+            ] ,
+        } ,
+    } ,
 ]
-WSGI_APPLICATION = 'centigram.wsgi.application'
-
+WSGI_APPLICATION='centigram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASES={
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'centigram',
-        'USER': 'feisal',
-        'PASSWORD':'1234',
+        'ENGINE': 'django.db.backends.postgresql' ,
+        'NAME': 'centigram' ,
+        'USER': 'feisal' ,
+        'PASSWORD': '1234' ,
     }
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS=[
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator' ,
+    } ,
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator' ,
+    } ,
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator' ,
+    } ,
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator' ,
+    } ,
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE='en-us'
 
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE='Africa/Nairobi'
 
-USE_I18N = True
+USE_I18N=True
 
-USE_L10N = True
+USE_L10N=True
 
-USE_TZ = True
-
+USE_TZ=True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join( BASE_DIR , 'media' )
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+django_heroku.settings( locals( ) )
