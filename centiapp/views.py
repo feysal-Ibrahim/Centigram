c71e66aecd92189598631251de581ba47cadb323
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-08 16:50:26 +0300

    create templates for comment

--- centiapp/views.py
+++ centiapp/views.py
@@ -1,138 +1,140 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render , redirect , get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse,Http404
-from .models import Image,Profile,Comment
-from .forms import EditProfileForm,UploadForm,CommentForm
+from django.http import HttpResponse , Http404
+from .models import Image , Profile , Comment
+from .forms import EditProfileForm , UploadForm , CommentForm
 from django.contrib.auth.models import User
 
+
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+@login_required( login_url='/accounts/login/' )
 def home(request):
-    current_user = request.user
-    profile = Profile.get_profile()
-    image = Image.get_images()
-    comments = Comment.get_comment()
-    return render(request,'index.html',{
-                                        "profile":profile,
-                                        "comments":comments,
-                                        "current_user":current_user,
-                                        "images":image,})
+    current_user=request.user
+    profile=Profile.get_profile( )
+    image=Image.get_images( )
+    comments=Comment.get_comment( )
+    return render( request , 'index.html' , {
+        "profile": profile ,
+        "comments": comments ,
+        "current_user": current_user ,
+        "images": image , } )
 
-@login_required(login_url='/accounts/login/')
+
+@login_required( login_url='/accounts/login/' )
 def profile(request):
-    current_user = request.user
-    profile = Profile.get_profile()
-    image = Image.get_images()
-    comments = Comment.get_comment()
-    return render(request,'profile/profile.html',{
-                                                  "comments":comments,
-                                                  "image":image,
-                                                  "user":current_user,
-                                                  "profile":profile,})
+    current_user=request.user
+    profile=Profile.get_profile( )
+    image=Image.get_images( )
+    comments=Comment.get_comment( )
+    return render( request , 'profile/profile.html' , {
+        "comments": comments ,
+        "image": image ,
+        "user": current_user ,
+        "profile": profile , } )
 
 
-@login_required(login_url='/accounts/login/')
+@login_required( login_url='/accounts/login/' )
 def settings(request):
-    title = 'Maxgram :-)'
-    settings = Profile.get_profile()
-    return render(request,'profile/setting.html',{"settings":settings,
-                                                    "title":title,})
+    title='Maxgram :-)'
+    settings=Profile.get_profile( )
+    return render( request , 'profile/setting.html' , {"settings": settings ,
+                                                       "title": title , } )
 
 
-
-@login_required(login_url='/accounts/login/')
+@login_required( login_url='/accounts/login/' )
 def edit(request):
-    current_user = request.user
+    current_user=request.user
     if request.method == 'POST':
-        form = EditProfileForm(request.POST,request.FILES)
-        if form.is_valid():
-            update = form.save(commit=False)
-            update.user = current_user
-            update.save()
-            return redirect('profile')
+        form=EditProfileForm( request.POST , request.FILES )
+        if form.is_valid( ):
+            update=form.save( commit=False )
+            update.user=current_user
+            update.save( )
+            return redirect( 'profile' )
     else:
-        form = EditProfileForm()
-    return render(request,'profile/edit.html',{
-                                                "form":form})
+        form=EditProfileForm( )
+    return render( request , 'profile/edit.html' , {
+        "form": form} )
 
-@login_required(login_url="/accounts/login/")
+
+@login_required( login_url="/accounts/login/" )
 def upload(request):
-    current_user = request.user
-    profiles = Profile.get_profile()
+    current_user=request.user
+    profiles=Profile.get_profile( )
     for profile in profiles:
         if profile.user.id == current_user.id:
             if request.method == 'POST':
-                form = UploadForm(request.POST,request.FILES)
-                if form.is_valid():
-                    upload = form.save(commit=False)
-                    upload.user = current_user
-                    upload.profile = profile
-                    upload.save()
-                    return redirect('home')
+                form=UploadForm( request.POST , request.FILES )
+                if form.is_valid( ):
+                    upload=form.save( commit=False )
+                    upload.user=current_user
+                    upload.profile=profile
+                    upload.save( )
+                    return redirect( 'home' )
             else:
-                form = UploadForm()
-            return render(request,'upload/new-upload.html',{
-                                                    "user":current_user,
-                                                    "form":form})
+                form=UploadForm( )
+            return render( request , 'upload/new-upload.html' , {
+                "user": current_user ,
+                "form": form} )
 
 
-
-@login_required(login_url="/accounts/login/")
+@login_required( login_url="/accounts/login/" )
 def search_results(request):
-    current_user = request.user
-    profile = Profile.get_profile()
+    current_user=request.user
+    profile=Profile.get_profile( )
     if 'username' in request.GET and request.GET["username"]:
-        search_term = request.GET.get("username")
-        searched_name = Profile.find_profile(search_term)
-        message = search_term
+        search_term=request.GET.get( "username" )
+        searched_name=Profile.find_profile( search_term )
+        message=search_term
 
-        return render(request,'search.html',{"message":message,
-                                             "profiles":profile,
-                                             "user":current_user,
-                                             "username":searched_name})
+        return render( request , 'search.html' , {"message": message ,
+                                                  "profiles": profile ,
+                                                  "user": current_user ,
+                                                  "username": searched_name} )
     else:
-        message = "You haven't searched for any user"
-        return render(request,'search.html',{"message":message})
+        message="You haven't searched for any user"
+        return render( request , 'search.html' , {"message": message} )
 
-@login_required(login_url='/accounts/login/')
-def new_comment(request,pk):
-    image = get_object_or_404(Image, pk=pk)
-    current_user = request.user
+
+@login_required( login_url='/accounts/login/' )
+def new_comment(request , pk):
+    image=get_object_or_404( Image , pk=pk )
+    current_user=request.user
     if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.image = image
-            comment.user = current_user
-            comment.save()
-            return redirect('home')
+        form=CommentForm( request.POST )
+        if form.is_valid( ):
+            comment=form.save( commit=False )
+            comment.image=image
+            comment.user=current_user
+            comment.save( )
+            return redirect( 'home' )
     else:
-        form = CommentForm()
-    return render(request, 'comment.html', {"user":current_user,
-                                            "comment_form":form})
+        form=CommentForm( )
+    return render( request , 'comment.html' , {"user": current_user ,
+                                               "comment_form": form} )
 
 
+@login_required( login_url="/accounts/login/" )
+def view_your_profile(request , pk):
+    current_user=request.user
+    image=Image.get_images( )
+    profile=Profile.get_profile( )
+    comment=Comment.get_comment( )
+    user=get_object_or_404( User , pk=pk )
+    return render( request , 'view.html' , {"user": current_user ,
+                                            "images": image ,
+                                            "my_user": user ,
+                                            "comments": comment ,
+                                            "profile": profile} )
 
-@login_required(login_url="/accounts/login/")
-def view_your_profile(request,pk):
-    current_user = request.user
-    image = Image.get_images()
-    profile = Profile.get_profile()
-    comment = Comment.get_comment()
-    user = get_object_or_404(User, pk=pk)
-    return render(request,'view.html',{"user":current_user,
-                                               "images":image,
-                                               "my_user":user,
-                                               "comments":comment,
-                                               "profile":profile})
 
-@login_required(login_url="/accounts/login/")
-def like(request,operation,pk):
-    image = get_object_or_404(Image,pk=pk)
+@login_required( login_url="/accounts/login/" )
+def like(request , operation , pk):
+    image=get_object_or_404( Image , pk=pk )
     if operation == 'like':
-        image.likes += 1
-        image.save()
-    elif operation =='unlike':
-        image.likes -= 1
-        image.save()
-    return redirect('home')
+        image.likes+=1
+        image.save( )
+    elif operation == 'unlike':
+        image.likes-=1
+        image.save( )
+    return redirect( 'home' )
