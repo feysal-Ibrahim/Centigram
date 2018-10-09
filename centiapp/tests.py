d8498bb4a4e8b1a9fc80a0a0698992c1e88b9db2
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-09 15:13:45 +0300

    add test fot the comment

--- centiapp/tests.py
+++ centiapp/tests.py
@@ -1,21 +1,17 @@
 from django.test import TestCase
-from .models import  Image,Comment
+from .models import Image , Comment
 
-#Create your tests here.
-class ImageTestClass(TestCase):
-   def setUp(self):
-       self.image=Image( image='image' , caption='caption' , upload_date='upload_date',likes='0' )
-       self.image.save( )
 
-   def test_instance(self):
+# Create your tests here.
+class ImageTestClass( TestCase ):
+    def setUp(self):
+        self.image=Image( image='image' , caption='caption' , upload_date='upload_date' , user='feysal',likes='0',profile='profile' )
+        self.image.save( )
+
+    def test_instance(self):
         self.assertTrue( isinstance( self.image , Image ) )
 
-   def test_saving_image(self):
-       self.image.save_image( )
-       images=Image.objects.all( )
-       self.assertTrue( len( images )>0 )
-
-
-
-
-
+    def test_saving_image(self):
+        self.image.save_image( )
+        images=Image.objects.all( )
+        self.assertTrue( len( images )>0 )
