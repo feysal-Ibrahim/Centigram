e9d8e2b84cc405a370e3a79998e8aae6b76b4692
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-09 18:07:57 +0300

    create test for the image

--- centiapp/tests.py
+++ centiapp/tests.py
@@ -1,17 +1,30 @@
 from django.test import TestCase
-from .models import Image , Comment
+from .models import Image , Comment , Profile
 
 
 # Create your tests here.
 class ImageTestClass( TestCase ):
+    # Set up method
     def setUp(self):
-        self.image=Image( image='image' , caption='caption' , upload_date='upload_date' , user='feysal',likes='0',profile='profile' )
+        self.profile=Profile.objects.create( name="profile" )
+        self.image=Image( image='image' , caption='caption' , upload_date='upload_date' , user='user' ,
+                          profile=self.profile )
+
         self.image.save( )
 
-    def test_instance(self):
-        self.assertTrue( isinstance( self.image , Image ) )
+    def save_method(self):
+        self.image.save( )
+        image=Image.objects.all( )
+        self.assertTrue( len( image )>0 )
 
-    def test_saving_image(self):
-        self.image.save_image( )
-        images=Image.objects.all( )
-        self.assertTrue( len( images )>0 )
+    # Testing save method
+    def save_image(self):
+        self.assertEqual( len( Image.objects.all( ) ) , 1 )
+
+    # Tear down method
+    def tearDown(self):
+        Image.objects.all( ).delete( )
+
+    def delete_image(self):
+        Image.delete_image_by_id( self.image.id )
+        self.assertEqual( len( Image.objects.all( ) ) , 0 )
